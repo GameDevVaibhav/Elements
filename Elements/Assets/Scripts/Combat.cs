18c1f580b6cd124df330438eb8dcb4f8154c6e90
GameDevVaibhav <147718788+GameDevVaibhav@users.noreply.github.com>
2024-01-22 20:08:15 +0530

    Slash Controlls Added

--- Elements/Assets/Scripts/Combat.cs
+++ Elements/Assets/Scripts/Combat.cs
@@ -16,32 +16,59 @@ public class Combat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Check for input to trigger the punch animation.
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            // Trigger the punch animation.
-            animator.SetTrigger("Punch");
-        }
 
-        // Check for input to trigger the kick animation.
-        if (Input.GetKeyDown(KeyCode.K) && !Input.GetKey(KeyCode.S))
+        if (animator.GetBool("isJumping"))
         {
-            // Trigger the kick animation.
-            animator.SetTrigger("Kick");
-        }
 
-        // Check for input to trigger the power_punch animation.
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.P))
-        {
-            // Trigger the power_punch animation.
-            animator.SetTrigger("Power_Punch");
         }
-
-        // Check for input to trigger the tackle animation.
-        if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.K))
+        else
         {
-            // Trigger the tackle animation.
-            animator.SetTrigger("Tackle");
+            // Check for input to trigger the punch animation.
+            if (Input.GetKeyDown(KeyCode.L) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.A))
+            {
+                // Trigger the punch animation.
+                animator.SetTrigger("Punch");
+            }
+
+            // Check for input to trigger the kick animation.
+            if (Input.GetKeyDown(KeyCode.K) && !Input.GetKey(KeyCode.S))
+            {
+                // Trigger the kick animation.
+                animator.SetTrigger("Kick");
+            }
+
+            // Check for input to trigger the power_punch animation.
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.L))
+            {
+                // Trigger the power_punch animation.
+                animator.SetTrigger("Power_Punch");
+            }
+
+            // Check for input to trigger the upper_slash animation.
+            if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.L))
+            {
+                // Trigger the upper_slash animation.
+                animator.SetTrigger("Upper_Slash");
+            }
+
+            if (Input.GetKey(KeyCode.A) && Input.GetKeyDown(KeyCode.L))
+            {
+                // Trigger the upper_slash animation.
+                animator.SetTrigger("Front_Slash");
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Q))
+            {
+                // Trigger the upper_slash animation.
+                animator.SetTrigger("Cross_Slash");
+            }
+            // Check for input to trigger the tackle animation.
+            if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.K))
+            {
+                // Trigger the tackle animation.
+                animator.SetTrigger("Tackle");
+            }
+
         }
     }
 }
